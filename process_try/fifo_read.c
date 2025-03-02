7580c96fbc2db8eca98302b7159d42434bbf52f3
Chuan81 <819937858@qq.com>
2025-03-02 17:03:21 +0800

    fifo_read&write

--- process_try/fifo_read.c
+++ process_try/fifo_read.c
@@ -10,33 +10,31 @@ int main(int argc, char const *argv[])
 {
     int fd;
     char *pipe_path = "/tmp/myfifo";
-    if(mkfifo(pipe_path,0664)!=0)
-    {
-        perror("mkfifo");
-        exit(EXIT_FAILURE);
-    }
-
     //对有名管道的特殊文件名
-    fd = open(pipe_path,O_WRONLY);
+    fd = open(pipe_path,O_RDONLY);
     if(fd==-1)
     {
         perror("open");
-        exit(EXIT_FAILURE);
+        if(errno!=17)
+        {
+            exit(EXIT_FAILURE);
+        }
     }
     char buf[100];
     ssize_t read_num;
-    while((read_num=read(STDIN_FILENO,buf,100))>0)
+    while ((read_num=read(fd,buf,100))>0)
     {
-        write(fd,buf,read_num);
+        write(STDOUT_FILENO,buf,read_num);
     }
-    if(read_num < 0)
+    
+    if(read_num <= 0)
     {
         perror("read");
         close(fd);
         exit(EXIT_FAILURE);
     }
-    printf("发送数据到管道完成");
-    
+    printf("接受管道数据完成");
+    close(fd);
     //释放管道
     if(unlink(pipe_path)==-1)
     {
